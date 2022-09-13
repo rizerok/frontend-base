9cf3d3495ea3d2a40259cc7c5baa017bf039559f
Alexey Bashinsky <bashinsky.alexey@gmail.com>
2022-09-13 03:12:12 +0300

    fix: react 18 render

--- src/index.tsx
+++ src/index.tsx
@@ -1,8 +1,10 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import { createRoot } from 'react-dom/client';
 import App from './components/app/index';
 
-ReactDOM.render(
-  <App/>,
-  document.getElementById('root')
-);
+const container = document.getElementById('root');
+if (!container) {
+  throw Error('container not found');
+}
+const root = createRoot(container);
+root.render(<App/>);
